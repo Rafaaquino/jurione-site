c1171842c16b33718950c0edc8d36037112502f2
Your NameRafael <rafaaquino28@gmail.com>
2026-01-12 16:03:17 -0300

    feat: configura deploy via pasta /docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfUse from "./pages/TermsOfUse";
-import LGPD from "./pages/LGPD";
+import { Toaster } from "@/components/ui/sonner";
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
+import PrivacyPolicy from "@/pages/PrivacyPolicy";
+import TermsOfUse from "@/pages/TermsOfUse";
+import LGPD from "@/pages/LGPD";
+import "./App.css";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/politica-privacidade" element={<PrivacyPolicy />} />
+        <Route path="/termos-uso" element={<TermsOfUse />} />
+        <Route path="/lgpd" element={<LGPD />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/politica-privacidade" element={<PrivacyPolicy />} />
-          <Route path="/termos-uso" element={<TermsOfUse />} />
-          <Route path="/lgpd" element={<LGPD />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+    </BrowserRouter>
+  );
+}
 
 export default App;
